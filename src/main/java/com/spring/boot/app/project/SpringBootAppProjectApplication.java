08aa9542dd7bbf1dbae6a2ccabc88e4fee99c6d2
santhosh <santhosh.k.te9@gmail.com>
2021-10-08 19:17:52 +0530

    first testing

---
+++ src/main/java/com/spring/boot/app/project/SpringBootAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.app.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//This Resister the class
+@SpringBootApplication
+public class SpringBootAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/app/project/SpringBootAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.app.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
